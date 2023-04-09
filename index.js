03dea1c1c3da36de484b503702f75aad901df534
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-04-09 21:24:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const button = document.getElementById('myButton');
+
+// button.addEventListener('click', function() {
+//   console.log('Button was clicked');
+// });
+
+
+// button.addEventListener('click',function(){
+// })
