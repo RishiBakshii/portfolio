acdaa7320dd80072973dd7085d4372231140f2b8
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-04-22 22:17:02 +0530

    added certificates section

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.getElementById("contact").addEventListener("click",function(){
-    alert("you clicked on contact")
-})
+// document.getElementById("contact").addEventListener("click",function(){
+//     alert("you clicked on contact")
+// })
