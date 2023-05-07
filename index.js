6761f31ef813400898dd9cb02c67c1aa0d860a97
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-05-07 18:42:37 +0530

    added breakpoints

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 let menuIcon=document.querySelector('#hamburger-menu')
+let navMenu=document.querySelector('#nav-menu')
 
 menuIcon.onclick= ()=> {
     menuIcon.classList.toggle('bx-x')
+
+    navMenu.classList.toggle('change')
 }
+
+// scroll detection
+window.onscroll = function() {
+    // navMenu.style.display='none'
+    menuIcon.classList.remove('bx-x')
+    navMenu.classList.remove('change')
+  };
+  
+
+
+// Attach a touchstart event listener to the document object
+navMenu.addEventListener("touchstart", function(event) {
+  // Check if the touch event occurred outside of the object
+  if (!navMenu.contains(event.navMenu)) {
+    // prompt("hello")
+  }
+
+});
+
+
