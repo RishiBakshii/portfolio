b61690e9bff6b1066c008d6c12695675b7432e97
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-05-10 16:15:18 +0530

    ui updates

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let menuIcon=document.querySelector('#hamburger-menu')
 let navMenu=document.querySelector('#nav-menu')
-
+let overlay=document.querySelector('#overlay')
 
 // click listerner on hamburger menu for openning the nav bar
 menuIcon.onclick= ()=> {
     menuIcon.classList.toggle('bx-x')
     navMenu.classList.toggle('change')
+    overlay.classList.toggle("isvisible")
+
 }
 
 // scroll detection -> for closing the navbar
@@ -13,6 +15,7 @@ window.onscroll = function() {
     // navMenu.style.display='none'
     menuIcon.classList.remove('bx-x')
     navMenu.classList.remove('change')
+    overlay.classList.remove("isvisible")
   };
   
 
@@ -25,5 +28,6 @@ navMenu.addEventListener("touchstart", function(event) {
   }
 
 });
+
 
 
