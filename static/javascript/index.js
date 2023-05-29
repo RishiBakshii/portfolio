41f7478f83ba0a0248800ea51ad6f545663627bc
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-05-29 22:58:28 +0530

    ui updates

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,31 +1,53 @@
-let menuIcon=document.querySelector('#hamburger-menu')
-let navMenu=document.querySelector('#nav-menu')
-let overlay=document.querySelector('#overlay')
+document.addEventListener('DOMContentLoaded', function() {
 
-// click listerner on hamburger menu for openning the nav bar
-menuIcon.onclick= ()=> {
-    menuIcon.classList.toggle('bx-x')
-    navMenu.classList.toggle('change')
-    overlay.classList.toggle("isvisible")
-
-}
-
-// scroll detection -> for closing the navbar
-window.onscroll = function() {
-    // navMenu.style.display='none'
-    menuIcon.classList.remove('bx-x')
-    navMenu.classList.remove('change')
-    overlay.classList.remove("isvisible")
-  };
-  
+  let menuIcon=document.querySelector('#hamburger-menu')
+  let navMenu=document.querySelector('#nav-menu')
+  let splashScreen=document.getElementById("splash-screen")
+  let splashText=document.getElementById("splashText")
 
 
-// Attach a touchstart event listener to the document object
-navMenu.addEventListener("touchstart", function(event) {
-  // Check if the touch event occurred outside of the object
-  if (!navMenu.contains(event.navMenu)) {
-    // prompt("hello")
+  // click listerner on hamburger menu for openning the nav bar
+  menuIcon.onclick= ()=> {
+      menuIcon.classList.toggle('bx-x')
+      navMenu.classList.toggle('change')
+      overlay.classList.toggle("isvisible")
+
   }
+
+  // scroll detection -> for closing the navbar
+  window.onscroll = function() {
+      // navMenu.style.display='none'
+      menuIcon.classList.remove('bx-x')
+      navMenu.classList.remove('change')
+      overlay.classList.remove("isvisible")
+    };
+    
+
+
+  // Attach a touchstart event listener to the document object
+  navMenu.addEventListener("touchstart", function(event) {
+    // Check if the touch event occurred outside of the object
+    if (!navMenu.contains(event.navMenu)) {
+    }
+
+  });
+
+
+  // const texts = ["Data-Cleaning",'PreProcessing','Exploratory Data Analysis','Feature Engineering','Modelling'];
+
+  // // Counter for tracking the current text
+  // let counter = 0;
+
+  // // Function to update the dynamic text
+  // function updateText() {
+  //   splashText.textContent = texts[counter];
+
+  //   // Increment the counter
+  //   counter = (counter + 1) % texts.length;
+  // }
+
+  // // Set interval to update the text every 2 seconds (2000 milliseconds)
+  // setInterval(updateText, 200);
 
 });
 
