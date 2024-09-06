6444d4a40e08e0b47785557e32d640d83721417f
rishibakshi <guitarguyrishi@gmail.com>
2024-09-06 19:39:13 +0530

    feat: Add new blog series

--- src/components/Blog/BlogList.tsx
+++ src/components/Blog/BlogList.tsx
@@ -4,10 +4,16 @@ export const BlogList = () => {
   return (
     <div className="flex flex-col gap-y-6">
             <BlogCard
-                desc={<p>In this blog, I cover critical thinking on <strong>state placement—does it belong here?</strong> 🤔 Can your app handle the re-renders? I also dive into using <strong>custom hooks for cleaner, maintainable code</strong>, while discussing the <strong>Single Responsibility Principle, SOLID principles, and key design patterns for scalable React apps</strong>. 💡 Plus, I explain the roles of <strong>Feature, UI, Page, and Compound components</strong>. 🚀 <br /> <br />I guarantee this blog will help you transition from junior-level to mid-to-senior level coding! 📈</p>}
-                image="https://cdn.hashnode.com/res/hashnode/image/upload/v1725524106168/2bd72e8f-d77c-4e3a-9feb-825e81102584.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp"
-                liveLink="https://rishibakshi.hashnode.dev/"
-                title="Master Design Patterns In React"
+                desc={<p>This series covers strategic state placement and using custom hooks to write cleaner, maintainable code. It dives into SOLID principles, design patterns, and explains the roles of Feature, UI, Page, and Compound components for scalable React apps.</p>}
+                image="https://cdn.hashnode.com/res/hashnode/image/upload/v1725610643755/8826f6a2-5aef-4f56-abff-3f6c8922fbe0.png?w=800&fit=crop&crop=entropy&auto=compress,format&format=webp"
+                liveLink="https://rishibakshi.hashnode.dev/series/master-design-patterns-in-react"
+                title="Master Design Patterns In React Series"
+            />
+            <BlogCard
+                desc={<p>This series uncovers common mistakes, misconceptions, and best practices in Next.js. Dive into real-world examples, tips, and optimizations to elevate your skills, enhance performance, and avoid pitfalls in your Next.js projects.</p>}
+                image="https://cdn.hashnode.com/res/hashnode/image/upload/v1725630763201/32cc3a04-60e6-4edd-9784-63676a0b2839.png"
+                liveLink="https://rishibakshi.hashnode.dev/series/you-dont-know-next-js"
+                title="You Don't Know Next.js Series"
             />
     </div>
   )
