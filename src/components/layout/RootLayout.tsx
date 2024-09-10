7ee4ae84f9e551d769f22b57cb22dd6003d21710
rishibakshi <guitarguyrishi@gmail.com>
2024-09-10 18:43:29 +0530

    feat: implement select-none propery in whole portfolio

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 export const RootLayout = () => {
   return (
     <>
-    <main className="w-screen h-screen bg-background overflow-x-hidden scroll-smooth">
+    <main className="w-screen h-screen bg-background overflow-x-hidden scroll-smooth select-none">
         <Outlet/>
     </main>
     </>
