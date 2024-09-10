eef5456d614f8ce4341b4152ca11c0f9b072d7b2
rishibakshi <guitarguyrishi@gmail.com>
2024-09-10 18:38:12 +0530

    feat: Add leetcode profile link

--- src/components/social/Socials.tsx
+++ src/components/social/Socials.tsx
@@ -1,5 +1,6 @@
 import { Github } from "../ui/icons/Github"
 import { HashNodeIcon } from "../ui/icons/HashNodeIcon"
+import { LeetCodeIcon } from "../ui/icons/LeetCodeIcon"
 import { Linkedin } from "../ui/icons/Linkedin"
 import { Twitter } from "../ui/icons/Twitter"
 
@@ -10,19 +11,18 @@ export const Socials = () => {
         <a target="_blank" href="https://github.com/RishiBakshii">
           <Github size={40}/>
         </a>
-
         <a target="_blank" href="https://www.linkedin.com/in/rishi-bakshi/">
           <Linkedin size={40}/>
         </a>
-
-        <a target="_blank" href="https://x.com/rishibakshiii">
-          <Twitter size={35}/>
+        <a target="_blank" href="https://leetcode.com/u/rishibakshii/">
+          <LeetCodeIcon size={35}/>
         </a>
-
         <a target="_blank" href="https://rishibakshi.hashnode.dev">
           <HashNodeIcon size={35}/>
         </a>
-
+        <a target="_blank" href="https://x.com/rishibakshiii">
+          <Twitter size={33}/>
+        </a>
     </div>
   )
 }
